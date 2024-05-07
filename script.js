74197ea3841faf469e94aed015191fbd40cfa687
kelvin-agramonte <kmagramonte@outlook.com>
2024-05-07 19:52:17 -0400

    Commit #7: Add scroll.

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ $(document).ready(function() {
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
     });
+
+    // Smooth Scrolling
+
+    $('a[href*="#"]').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+    );
+    });
 });
 
